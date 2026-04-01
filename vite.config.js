873bf4791c0cf191f7a566a729f946467038a9f3
Leszinzinsgardois <139361498+Leszinzinsgardois@users.noreply.github.com>
2026-04-01 15:18:48 +0200

    chore(deploy): switch to gh-pages package instead of github actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/web-team-grx/', // Indispensable pour un bon routage sur Github Pages
   build: {
     sourcemap: false, // Désactive la génération des source maps pour la prod (sécurité)
     target: 'esnext',
